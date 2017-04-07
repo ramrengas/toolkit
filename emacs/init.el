dea2316a37bba104a638318057f9765d91d37299
Ram Rengaswamy <ramr@beeswax.com>
2017-04-07 13:22:44 -0400

    Markdown

--- emacs/init.el
+++ emacs/init.el
@@ -63,6 +63,15 @@
 ;; Dockerfile
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 
+;; Markdown
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
